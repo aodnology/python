cf01282cb5fdae560d601d1d0c25beee5c6b9541
Sooho park <aodnology@gmail.com>
2022-10-19 18:59:05 +0900

    2022.10.19

--- 학교 수업/빅데이터프로그래밍1장.py
+++ 학교 수업/빅데이터프로그래밍1장.py
@@ -4,35 +4,29 @@ print("1. ", a)         # : 'python' == 문법적으로 의미를 가지는 단�
                         # : "python" == 크게 의미를 가지지 않는 문자열 
                         # 사실 큰 의이 없음
 
-print('\n')
-
 #2. 복소수 지원
 a = 2 + 3j
 b = 3
 print('2. ', a * b) # 콤마(,)는 자바 (+) 의 의미와 같
 
-print('\n')
 
 #3. 조건문(if)
 a = 3
 if a > 1:
     print("3. ", "a is greater than 1")
 
-print('\n')
 
 #4. 반복문(for)
 print('4. ')
 for a in [1,2,3]: # 리스트([]) 안의 값을 a 안에 넣어 하나씩 꺼내 사용함
     print(a)
 
-print('\n')
 
 #4-2. 반복문(for) 응용 리스트 문자열 한줄로 춮력
 print('4-2. ')
 for a in [1,2,3]: # 리스트([]) 안의 값을 a 안에 넣어 하나씩 꺼내 사용함
     print(a, end=' ') # end=''에서 ''안에 특정 값을 입력하면 요소 사이에 그 값이 입력됨
 
-print('\n')
 
 #5. 반복문 while
 print('5. ')
@@ -41,7 +35,6 @@ while i < 3:    #조건식
     i = i+1     #증감식 
     print(i)    #과 같은 기본 while문의 구조와 같음
 
-print('\n')
 
 #5-2. 반복문 while
 print('5-2.')
@@ -50,7 +43,6 @@ while i < 3:    #조건식
     i = i+1     #증감식 
     print(i, end=' ') #for 반복문과 마찬가지로 end
 
-print('\n')
 
 #6. 함수(def)
 def sum(a, b):
