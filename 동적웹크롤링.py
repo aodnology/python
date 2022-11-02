a12beb9b5bef77e083b34250c997ef012c08d3ba
Sooho park <aodnology@gmail.com>
2022-11-02 10:45:08 +0900

    2022.11.02

--- 동적웹크롤링.py
+++ 동적웹크롤링.py
@@ -18,3 +18,24 @@ def bean_store(result):
             html = wd.page_source
             soupCB = Beautifulsoup(html, 'html.parser')
             store_name_h2 = soupCB.select("div.store_txt > h2")
+            store_name = store_name_h2[0].string
+            print(store_name)
+            store_info = soupDB.select("div.store_txt > table.store_table > tbody > tr > td")
+            store_address_list = list(store_info[2])
+            store_address = store_address_list[0]
+            store_phone = store_info[3].string
+            result.append([store_name]+[store_address]+[store_phone])
+        except:
+            continue
+    return
+
+def main():
+    result = []
+    print("coffee bean crawling")
+    bean_store(result)
+
+    CB_tb1 = pd.DataFrame(result, columns=('store', 'address', 'phone'))
+    CB_tb1.to_csv('./CoffeeBean.csv', encoding='cp949',mode='w', index=True)
+
+if __name__ == '__main__':
+    main()
