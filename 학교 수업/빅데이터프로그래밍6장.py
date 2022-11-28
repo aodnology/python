a06d198e13dded711b13472c19d16f9046b712cf
trap909 <aodnology@gmail.com>
2022-11-28 21:18:28 +0900

    2022.11.28

--- 학교 수업/빅데이터프로그래밍6장.py
+++ 학교 수업/빅데이터프로그래밍6장.py
@@ -1,12 +1,50 @@
-def gugu(n):
-    result = []
+# 함수 이용하기
+
+def gugu(n): 
+    result = []             
     i = 1
     while i <10:
         result.append(n * i)
         i += 1
-        return result
+    return result
 
+''' 
+3 : 함수 선언
+4 : result 변수명의 리스트 자료형 생성
+5 : 1~9까지 대입할 변수[i] 선언
+6 : i < 10 으로 9단 까지 반복
+7 : 리스트 자료형에 append() 함수를 활용하여 값 추가
+8 : i 1씩 증가
+9 : 구구단 값이 담기는 리스트 자료형 변수에 결과값 넣기
+'''
+print(gugu(2))
 
+# 3과 5의 배수 더하기
+hap = 0
+for i in range(1, 100):
+    if i % 3 == 0 or i % 5 == 0:
+        hap += i
+print(hap)
 
-def getTatalPage(m, n):
-    return m // n+1
+#게시판 페이징하기
+'''
+게시물의 총 건수와 페이지에 보여줄 게시물 수를 입력으로 주었을 때
+총 페이지수를 출력하는 프로그램
+'''
+def getTotalPage(m,n):
+    if m % n == 0:
+        return m // n
+    else :
+        return m // n + 1
+
+'''
+34 : m == 게시물의 총 건수
+     n == 페이지당 보여줄 게시물 수
+     총 페이지수 = 총 건수 / 한 페이지당 보여줄 건수 + 1
+'''
+# print(getTotalPage(5,10))
+# print(getTotalPage(15,10))
+# print(getTotalPage(25,10))
+# print(getTotalPage(35,10))
+
+# 메모장
--- 학교 수업/빅데이터프로그래밍9_1장.py
+++ 학교 수업/빅데이터프로그래밍9_1장.py
@@ -5,17 +5,47 @@ import numpy as np
 s = pd.Series([1,3,5,np.nan,6,8])
 '''
 Pandas에서 Series란? 
-(
+:
 Series는 어떤 데이터 타입이든 보유할 수 있는 레이블(label)링된 1차원 배열
 //레이블링이란 주어진 데이터에 정답지를 만들어주는 작업이고, 이때 정답지를 레이블이라한다.
-)
+
 
 NaN (결측값) 이란?
-(
+:
 Not a Number의 약어
 표현 불가능한 수치형 결과를 의미함.
-)
+
 '''
 
+# 날짜형 데이터 생성
 dates = pd.date_range('20220202', periods=6)
 print(dates)
+'''
+data_range() : 날짜형 데이터 함수
+periods=[숫자]
+
+2022년2월2일 기준으로, 6일동안의 기간을 dates 변수에 지정함
+'''
+
+# Dataframe 유형 데이터 생성
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=['A','B','C','D'])
+print(df)
+'''
+np.random.randn(6,4) : 표준 정규 분포에서 난수 matrix array 생성
+index=date : index(행)을 date(앞에 만든 날짜형 데이터) 대입
+column=['A','B','C','D'] : 열로 지정
+'''
+
+# 데이터 확인
+df.head(3)
+# print(df.head(3))
+# head() 함수를 사용하면 입력된 값 만큼(3)의 정보 확인가능, 기본적으로는 5행 까지 보여줌
+
+# 데이터의 컬럼, 인덱스, 내용 확인
+df.index   #컬럼
+df.columns #인덱스
+df.values  #내용
+
+# DataFrame 개요 확인
+df.info()
+
